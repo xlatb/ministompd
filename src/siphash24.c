e1dd82c3086838a20649d9449e335343cd14c0b1
Andrew Wendt <andy@putergeek.com>
2022-02-03 16:12:38 -0800

    Silence fallthrough warnings

--- src/siphash24.c
+++ src/siphash24.c
@@ -85,21 +85,29 @@ int siphash_24_crypto_auth( unsigned char *out, const unsigned char *in, unsigne
 
   switch( left )
   {
-  case 7: b |= ( ( u64 )in[ 6] )  << 48;
-
-  case 6: b |= ( ( u64 )in[ 5] )  << 40;
-
-  case 5: b |= ( ( u64 )in[ 4] )  << 32;
-
-  case 4: b |= ( ( u64 )in[ 3] )  << 24;
-
-  case 3: b |= ( ( u64 )in[ 2] )  << 16;
-
-  case 2: b |= ( ( u64 )in[ 1] )  <<  8;
-
-  case 1: b |= ( ( u64 )in[ 0] ); break;
-
-  case 0: break;
+  case 7:
+    b |= ( ( u64 )in[ 6] )  << 48;
+    // Fallthrough
+  case 6:
+    b |= ( ( u64 )in[ 5] )  << 40;
+    // Fallthrough
+  case 5:
+    b |= ( ( u64 )in[ 4] )  << 32;
+    // Fallthrough
+  case 4:
+    b |= ( ( u64 )in[ 3] )  << 24;
+    // Fallthrough
+  case 3:
+    b |= ( ( u64 )in[ 2] )  << 16;
+    // Fallthrough
+  case 2:
+    b |= ( ( u64 )in[ 1] )  <<  8;
+    // Fallthrough
+  case 1:
+    b |= ( ( u64 )in[ 0] );
+    break;
+  case 0:
+    break;
   }
 
 #ifdef DEBUG
